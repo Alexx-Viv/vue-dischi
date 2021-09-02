317d9dbcd16bda328cd663fca697cb5f98f03e29
Alexx-Viv <81645879+Alexx-Viv@users.noreply.github.com>
2021-09-02 14:59:43 +0200

    Api request + diplay

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
+console.log('vue', Vue);
 Vue.config.devtools = true;
 
 const app = new Vue({
-  let: '#app',
-  data: {},
-  methods: {},
+  el: '#app',
+  data: {
+    albumList: [],
+  },
+  methods: {
+    see() {
+      console.log(this.albumList);
+    },
+  },
+  created() {
+    axios
+      .get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then((response) => {
+        console.log(response);
+        this.albumList = response.data.response;
+        console.log(this.albumList);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
 });
