29c31a51abcc90225288aab065d405e2cc5b29d5
Alexx-Viv <81645879+Alexx-Viv@users.noreply.github.com>
2021-09-02 15:53:51 +0200

    Add Bonus

--- js/app.js
+++ js/app.js
@@ -5,10 +5,22 @@ const app = new Vue({
   el: '#app',
   data: {
     albumList: [],
+    genreList: ['all'],
+    filteredList: [],
   },
   methods: {
     see() {
       console.log(this.albumList);
+      console.log(this.genreList);
+    },
+    filterList(genre) {
+      let filter = genre;
+      console.log(filter);
+      this.filteredList = this.albumList.filter((list, index) => {
+        if (list.genre === filter || filter === 'all') {
+          return true;
+        }
+      });
     },
   },
   created() {
@@ -16,8 +28,14 @@ const app = new Vue({
       .get('https://flynn.boolean.careers/exercises/api/array/music')
       .then((response) => {
         console.log(response);
-        this.albumList = response.data.response;
+        this.albumList = this.filteredList = response.data.response;
         console.log(this.albumList);
+        for (let i = 0; i < this.albumList.length; i++) {
+          if (!this.genreList.includes(this.albumList[i].genre)) {
+            this.genreList.push(this.albumList[i].genre);
+          }
+        }
+        console.log(this.genreList);
       })
       .catch((err) => {
         console.log(err);
